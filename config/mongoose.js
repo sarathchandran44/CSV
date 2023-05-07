2c910caab7a13b4047ad959caf493ba957c748fa
TOSIF <tosifik2000@gmail.com>
2023-05-07 07:39:03 -0500

    Added required files

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+
+
+
+// mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+mongoose.connect("mongodb+srv://admin-tosif:Test123@cluster0.7rdhg14.mongodb.net/csvUploads",{useNewUrlParser:true});
+
+const db = mongoose.connection;
+
+db.once('open', function(){
+    console.log('database connected to the server successfully!');
+});
+
+module.exports = db;
