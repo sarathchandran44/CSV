ffd8bb07ff563ade0fba276f4716aa7b1a9659ff
Tosif145 <104226018+Tosif145@users.noreply.github.com>
2023-05-08 03:49:00 -0500

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 
-// mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+// mongoose.connect('mongodb://127.0.0.1:27017/csv-Uploads');
 mongoose.connect("mongodb+srv://admin-tosif:Test123@cluster0.7rdhg14.mongodb.net/csvUploads",{useNewUrlParser:true});
 
 const db = mongoose.connection;
